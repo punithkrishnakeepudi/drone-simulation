1d761f7c675ea741a0248347631b2eb67093d32b
Punith Krishna <punithkrishna147@gmail.com>
2026-08-25 04:18:15 +0530

    Configure BACKEND_URL to Render service

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 // Set this to your Render backend URL when deploying.
 // Example: 'https://my-drone-api.onrender.com'
 // Leave blank '' to connect to the same host (for local testing).
-export const BACKEND_URL = ''; 
+export const BACKEND_URL = 'https://drone-simulation-7iws.onrender.com'; 
